5c40ee3151b062eb2058a8cf4336becb5d59a85b
Kaseiinov_I <izlax6002@gmail.com>
2025-08-08 00:47:03 +0600

    created main controller

---
+++ src/main/java/kg/attractor/jobsearch/controller/MainController.java
@@ -0,0 +1,19 @@
+package kg.attractor.jobsearch.controller;
+
+import kg.attractor.jobsearch.service.VacancyService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+@RequiredArgsConstructor
+public class MainController {
+    private final VacancyService vacancyService;
+
+    @GetMapping
+    public String vacancies(Model model) {
+        model.addAttribute("vacancies", vacancyService.findAllActive());
+        return "employer/vacancies";
+    }
+}
