b9089933b745bc06c2bedd5bd729fbc926a75196
Kaseiinov_I <izlax6002@gmail.com>
2025-08-17 00:13:06 +0600

    Added pageable to vacancies

--- src/main/java/kg/attractor/jobsearch/controller/MainController.java
+++ src/main/java/kg/attractor/jobsearch/controller/MainController.java
@@ -5,15 +5,17 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequiredArgsConstructor
 public class MainController {
     private final VacancyService vacancyService;
 
-    @GetMapping
-    public String vacancies(Model model) {
-        model.addAttribute("vacancies", vacancyService.findAllActive());
-        return "employer/vacancies";
-    }
+//    @GetMapping
+//    public String vacancies(@Page
+//                            Model model) {
+//        model.addAttribute("vacancies", vacancyService.findAllActive( page,  size));
+//        return "employer/vacancies";
+//    }
 }
--- src/main/java/kg/attractor/jobsearch/service/VacancyService.java
+++ src/main/java/kg/attractor/jobsearch/service/VacancyService.java
@@ -2,6 +2,8 @@ package kg.attractor.jobsearch.service;
 
 import kg.attractor.jobsearch.dto.UserDto;
 import kg.attractor.jobsearch.dto.VacancyDto;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 import java.util.List;
 
@@ -18,7 +20,7 @@ public interface VacancyService {
 
     List<VacancyDto> findAll();
 
-    List<VacancyDto> findAllActive();
+    Page<VacancyDto> findAllActive(Pageable pageable);
 
     List<VacancyDto> findByCategory(String category);
 
